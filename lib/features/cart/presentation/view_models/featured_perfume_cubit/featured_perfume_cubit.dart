42bc16ed96a1d61fbdd182d59edfd596d6247144
Yazan Mtawej <yznmtwj@gmail.com>
2025-06-15 03:38:50 +0300

    create featch Feature Perfume method

--- lib/features/cart/presentation/view_models/featured_perfume_cubit/featured_perfume_cubit.dart
+++ lib/features/cart/presentation/view_models/featured_perfume_cubit/featured_perfume_cubit.dart
@@ -1,9 +1,26 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:onestopshop/features/Home/data/models/perfume_model.dart';
+import 'package:onestopshop/features/Home/data/models/repos/home_repo.dart';
 
 part 'featured_perfume_state.dart';
 
 class FeaturedPerfumeCubit extends Cubit<FeaturedPerfumeState> {
-  FeaturedPerfumeCubit() : super(FeaturedPerfumeInitial());
+  FeaturedPerfumeCubit(this.homeRepo) : super(FeaturedPerfumeInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> featchFeaturePerfume() async {
+    emit(FeaturedPerfumeLoading());
+    var result = await homeRepo.featchFeaturePerfume();
+    result.fold(
+      (failure) {
+        emit(FeaturedPerfumeFailure(failure.errMessage));
+      },
+      (perfume) {
+        emit(FeaturedPerfumeSuccess(perfume: perfume));
+      },
+    );
+  }
 }
