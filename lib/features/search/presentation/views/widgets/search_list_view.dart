78243bb608c4d45f24e1b6afd78249ec0dc85cdf
Yazan Mtawej <yznmtwj@gmail.com>
2025-08-02 23:33:54 +0300

    edit search view design

--- lib/features/search/presentation/views/widgets/search_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_list_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onestopshop/constents.dart';
 import 'package:onestopshop/features/Home/presentation/views/widgets/perfum_list_view_item.dart';
 import 'package:onestopshop/features/search/presentation/view_models/cubit/search_cubit.dart';
-
 
 class SearchListView extends StatelessWidget {
   const SearchListView({super.key});
@@ -16,15 +16,101 @@ class SearchListView extends StatelessWidget {
         }
 
         return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 20),
           itemCount: perfumes.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: PerfumeListViewItem(perfume: perfumes[index]),
+            return _AnimatedSearchItem(
+              delay: Duration(milliseconds: 100 * index),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 7),
+                child: Material(
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(18),
+                  color: Colors.black,
+                  shadowColor: kCardColor,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          kButtonColor,
+                          kCardColor.shade100,
+                          kCardColor.shade200,
+                          kCardColor.shade700,
+                          kCardColor.shade100,
+                          kCardColor.shade200,
+                          kCardColor.shade700,
+                          kIconColor,
+                        ],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: PerfumeListViewItem(perfume: perfumes[index]),
+                    ),
+                  ),
+                ),
+              ),
             );
           },
         );
       },
+    );
+  }
+}
+
+class _AnimatedSearchItem extends StatefulWidget {
+  final Widget child;
+  final Duration delay;
+
+  const _AnimatedSearchItem({required this.child, required this.delay});
+
+  @override
+  State<_AnimatedSearchItem> createState() => _AnimatedSearchItemState();
+}
+
+class _AnimatedSearchItemState extends State<_AnimatedSearchItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fade;
+  late Animation<Offset> _slide;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _fade = Tween<double>(
+      begin: 0.2,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    _slide = Tween<Offset>(
+      begin: const Offset(0, 0.2),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    Future.delayed(widget.delay, () {
+      if (mounted) _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _fade,
+      child: SlideTransition(position: _slide, child: widget.child),
     );
   }
 }
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -13,6 +13,7 @@ class SearchViewBody extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        backgroundColor: kIconColor.shade100,
         appBar: AppBar(
           backgroundColor: kAppBarColor,
           elevation: 0,
@@ -30,14 +31,53 @@ class SearchViewBody extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: TextField(
                   controller: controller,
-                  decoration: const InputDecoration(
-                    hintText: 'Search',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(),
-                  ),
                   onChanged: (value) {
                     context.read<SearchCubit>().search(keyword: value);
                   },
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Search for perfumes...',
+                    hintStyle: const TextStyle(color: kIconColor),
+                    suffixIcon: Container(
+                      margin: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent.shade100.withAlpha(40),
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(Icons.search, color: Colors.blueAccent),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 18,
+                      horizontal: 20,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(color: Colors.black, width: 3),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(
+                        color: Colors.blueAccent,
+                        width: 4,
+                      ),
+                    ),
+                    prefixIcon: IconButton(
+                      icon: const Icon(
+                        size: 20,
+                        Icons.delete_sharp,
+                        color: Colors.blueAccent,
+                      ),
+                      onPressed: () {
+                        controller.clear();
+                        context.read<SearchCubit>().clear();
+                      },
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
@@ -49,7 +89,7 @@ class SearchViewBody extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 10),
-             Expanded(child: const SearchListView()),
+              Expanded(child: const SearchListView()),
             ],
           ),
         ),
